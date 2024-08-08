c3f5dd098b1256e99e96e79619e199841cc151eb
Hector-Arthur <126472496+Hector-Arthur@users.noreply.github.com>
2024-08-08 11:49:29 -0300

    Correção

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Sobre seu estilo de jogar...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
